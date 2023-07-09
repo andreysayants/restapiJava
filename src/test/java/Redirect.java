77623222ad18acf3b96268ac5627ae3f64be1d90
Саянц Андрей Владимирович (4082282) <asayanc@vtb.ru>
2023-07-09 13:16:30 +0300

    Добавил Redirect

---
+++ src/test/java/Redirect.java
@@ -0,0 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Redirect {
+
+    @Test
+    public void testRedirect() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+//        response.prettyPrint();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+    }
+}
