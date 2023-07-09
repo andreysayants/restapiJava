56b26f839f9e44a912145c14a0d875a3adcb0b61
Саянц Андрей Владимирович (4082282) <asayanc@vtb.ru>
2023-07-09 15:53:05 +0300

    Добавил Tokens

---
+++ src/test/java/Tokens.java
@@ -0,0 +1,40 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+public class Tokens {
+
+    @Test
+    public void testTokens() throws InterruptedException {
+        String result = null;
+
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+
+        String token = response.get("token");
+        int seconds = response.get("seconds");
+        System.out.println("Токен " + token);
+        System.out.println("Количество секунд, через сколько задача будет выполнена " + seconds);
+
+        while (result == null) {
+            JsonPath response2 = RestAssured
+                    .given()
+                    .queryParam("token", token)
+                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                    .jsonPath();
+            String status = response2.get("status");
+            if (status.equals("Job is NOT ready")) {
+                System.out.println("Job is NOT ready");
+                Thread.sleep(seconds * 1000);
+            } else if (status.equals("Job is ready")) {
+                System.out.println("Job is ready");
+                result = response2.get("result");
+                System.out.println("Результат " + result);
+            }
+
+        }
+    }
+}
+
