4728ebe6721d49186f27f00316fbbaf24afdcd52
Саянц Андрей Владимирович (4082282) <asayanc@vtb.ru>
2023-07-09 12:44:46 +0300

    Добавил ParsingJSON

---
+++ src/test/java/ParsingJSON.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class ParsingJSON {
+
+    @Test
+    public void testParsingJSON(){
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        ArrayList answer = response.get("messages");
+        System.out.println(answer.get(1));
+    }
+}
