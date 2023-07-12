db1e5ce06e94ec834a3d280ae1ab1aa2806fc9f1
Саянц Андрей Владимирович (4082282) <asayanc@vtb.ru>
2023-07-12 10:17:52 +0300

    Добавил ParameterizedTest в ShortPhrase

--- src/test/java/ShortPhrase.java
+++ src/test/java/ShortPhrase.java
@@ -1,21 +1,19 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ShortPhrase {
 
-    @Test
-    public void testShortPhraseNegative() {
-        String shortWord = "qwerty";
+    @ParameterizedTest
+    @ValueSource(strings = {"qwerty", "1234567891013141234243534253246"})
+    public void testShortPhrase(String word) {
 
-        assertFalse(shortWord.length() > 15, "Текст должен быть длиннее 15 символов");
-    }
-
-    @Test
-    public void testShortPhrasePostitive() {
-        String longWord = "1234567891013141234243534253246";
-
-        assertTrue(longWord.length() > 15, "Текст должен быть длиннее 15 символов");
+        if (word.length() > 15) {
+            assertTrue(word.length() > 15, "Текст должен быть длиннее 15 символов");
+        } else {
+            assertFalse(word.length() > 15, "Текст должен быть длиннее 15 символов");
+        }
     }
 }
